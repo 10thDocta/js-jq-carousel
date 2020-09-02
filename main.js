6df8e8ebe5d8a88010eafeeea3a0498c7521419f
Davide Preti <davide.preti86@gmail.com>
2020-09-02 16:55:19 +0200

    [Carosello] Migliorata logica. Funziona sia per img che per pallini

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
 $(document).ready(function () {
 
-    // metodo per avanzare in avanti nelle immagini
+    // metodo per avanzare in avanti nelle immagini cliccando sulla freccia destra
     $(".next > i").click(function () {
 
-        var imgActive = $("img.active");
+        var currentActive = $(".active");
 
-        $(imgActive).removeClass("active");
+        $(currentActive).removeClass("active");
 
-        if ($(imgActive).hasClass("last")) { $("img.first").addClass("active"); }
-        else { $(imgActive).next().addClass("active"); }
-
-
+        if ($(currentActive).hasClass("last")) {
+            $(".first").addClass("active");
+        }
+        else {
+            $(currentActive).next().addClass("active");
+        }
     });
 
 
-    // metodo per tornare indietro nelle immagini
+    // metodo per tornare indietro nelle immagini cliccando sulla freccia sinistra
     $(".prev > i").click(function () {
 
-        var imgActive = $("img.active");
+        var currentActive = $(".active");
 
-        $(imgActive).removeClass("active");
+        $(currentActive).removeClass("active");
 
-        if ($(imgActive).hasClass("first")) { $("img.last").addClass("active"); }
-        else { $(imgActive).prev().addClass("active"); }
-
-
+        if ($(currentActive).hasClass("first")) {
+            $(".last").addClass("active");
+        }
+        else {
+            $(currentActive).prev().addClass("active");
+        }
     });
-
-
-
-
-
-
 
 
 
